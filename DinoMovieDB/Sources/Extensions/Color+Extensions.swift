f3c14cfc5476791915e3a51e5717b23263b4ebcf
Mario Vanegas <mvanegasdev@gmail.com>
2021-03-03 21:57:57 -0600

    🌐 Adds login localization

---
+++ DinoMovieDB/Sources/Extensions/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  DinoMovieDB
+//
+//  Created by Mario Vanegas on 3/3/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundColor: Color = Color("background-color")
+    static let primaryColor: Color = Color("primary-color")
+    static let tmdbBackgroundControls: Color = Color("tmdb-background-controls")
+    static let tmdbPlaceholder: Color = Color("tmdb-placeholder")
+}
