79f8dd35dc0fbfc3f1cfe7c3b1a548e6109b14fd
Mario Vanegas <mvanegasdev@gmail.com>
2021-03-19 23:54:59 -0600

    💄 Designs profile swiftUI view

---
+++ DinoMovieDB/Sources/Presentation/Views/ProfileView.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileView.swift
+//  DinoMovieDB
+//
+//  Created by Mario Vanegas on 3/19/21.
+//
+
+import SwiftUI
+import Kingfisher
+
+// TODO: Fix logout button since this is just a temporary one
+struct ProfileView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 38) {
+            profile
+            Button("Logout") {
+                
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 16)
+        .padding(.vertical, 18)
+        .background(Color(R.color.backgroundColor.name))
+    }
+    
+    private var profile: some View {
+        HStack(alignment: .top, spacing: 20) {
+            KFImage(URL(string: "https://happytravel.viajes/wp-content/uploads/2020/04/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.pg"))
+                .placeholder {
+                    Image(systemName: "person")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                        .foregroundColor(.white)
+                }
+                .resizable()
+                .scaledToFit()
+                .frame(width: 75, height: 75)
+                .background(Color.black)
+                .clipShape(Circle())
+                .shadow(color: Color(R.color.tmdbShadow.name), radius: 10, x: 0, y: 5)
+            VStack(alignment: .leading) {
+                Text("Mario Vanegas")
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(R.color.defaultAccent.name))
+                Text("vanegasdev")
+                    .font(.system(size: 18))
+                    .foregroundColor(Color(R.color.secondarySolid.name))
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
